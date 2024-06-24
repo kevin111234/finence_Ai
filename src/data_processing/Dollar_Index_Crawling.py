54a962483c4a579e338d52f3176e29bcd3cf993c
woghd111234 <jaehong243587@gmail.com>
2024-06-25 02:59:10 +0900

    환율정보 크롤링 후 html 파싱 기능 작성 완료

--- src/data_processing/Dollar_Index_Crawling.py
+++ src/data_processing/Dollar_Index_Crawling.py
@@ -9,11 +9,22 @@ from datetime import datetime, timedelta
 today_date = datetime.now()
 
 # 크롤링할 주소 설정
-base_url = 'https://finance.naver.com/marketindex/exchangeDailyQuote.naver?marketindexCd=FX_USDKRW'
+base_url = 'https://finance.naver.com/marketindex/exchangeDailyQuote.naver?marketindexCd=FX_USDKRW&page='
+urls = []
+for i in range(1): # 나중에 37로 수정하기
+    page_url = f'{base_url}{i+1}'
+    urls.append(page_url)
+print(urls)
 
 # 크롤링 수행
-for page in range(37):
-    url = f'{base_url}&page={page}'
+for url in urls:
+    # 요청 보내기
+    response = requests.get(url)
+    # HTML 파싱
+    soup = BeautifulSoup(response.content, "html.parser")
+    # 파싱한 soup 변수에서 div > table > tbody > <tr class="up">, <tr class="down"> 모두 정리하기
+    print(soup)
+
 
 # 달러 인덱스 크롤링
 
