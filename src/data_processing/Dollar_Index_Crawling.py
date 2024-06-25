9048009cb9ff90ca1632daef78772c903ba5c9c8
woghd111234 <jaehong243587@gmail.com>
2024-06-25 10:38:45 +0900

    달러인덱스 기준 시간과 수치 크롤링 기능 작성완료

--- src/data_processing/Dollar_Index_Crawling.py
+++ src/data_processing/Dollar_Index_Crawling.py
@@ -42,6 +42,16 @@ exchange_rate_df = pd.DataFrame({
 # print(exchange_rate_df)
 
 # 달러 인덱스 크롤링
+dollar_index_url = 'https://kr.investing.com/currencies/us-dollar-index'
+response = requests.get(dollar_index_url)
+# HTML 파싱
+soup = BeautifulSoup(response.content, "html.parser")
+dollar_index_value = soup.find("span", {"id": "last_last"})
+time_index = soup.select_one("#quotes_summary_current_data > div.instrumentDataDetails > div.left.current-data > div.bottom.lighterGrayFont.arial_11 > span.bold.pid-8827-time")
+if dollar_index_value:
+    print(f"시간: {time_index.text.strip()} 달러 인덱스 값: {dollar_index_value.text.strip()}")
+else:
+    print("달러 인덱스 값을 찾을 수 없습니다.")
 
 # 데이터 가공
 
