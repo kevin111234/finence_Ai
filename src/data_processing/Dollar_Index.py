160531d046c6914f44b0a4854368b29afe620b35
woghd111234 <jaehong243587@gmail.com>
2024-06-28 03:05:19 +0900

    환율, 달러인덱스 정보 slack에 알림 보내기 기능 구현

--- src/data_processing/Dollar_Index.py
+++ src/data_processing/Dollar_Index.py
@@ -10,9 +10,23 @@ def DollarIndex():
     dollar_index_value = soup.find("span", {"id": "last_last"})
     time_index = soup.select_one("#quotes_summary_current_data > div.instrumentDataDetails > div.left.current-data > div.bottom.lighterGrayFont.arial_11 > span.bold.pid-8827-time")
     if dollar_index_value:
-        result = f"현재 달러 인덱스 값: {dollar_index_value.text.strip()}"
+        result = f"{dollar_index_value.text.strip()}"
     else:
         result = "달러 인덱스 값을 찾을 수 없습니다."
     str(result)
     return result
 # print(result)
+
+def DollarRate(tag):
+    dollar_rate_url = 'https://finance.naver.com/marketindex/exchangeDailyQuote.naver?marketindexCd=FX_USDKRW&page=1'
+    response = requests.get(dollar_rate_url)
+    # HTML 파싱
+    soup = BeautifulSoup(response.content, "html.parser")
+    cells_date = soup.select_one("div > table > tbody > tr:nth-child(1) > td:nth-child(1)")
+    cells_num = soup.select_one("div > table > tbody > tr:nth-child(1) > td:nth-child(2)")
+    cells_date = cells_date.text.strip()
+    cells_num = cells_num.text.strip()
+    if tag == "date":
+        return cells_date
+    if tag == "num":
+        return cells_num
--- src/web/backend/slack_manager.py
+++ src/web/backend/slack_manager.py
@@ -3,6 +3,32 @@ import os, sys
 from dotenv import load_dotenv
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
 from data_processing import Dollar_Index
+import json
+import requests
+
+def notice_message(token, channel, text, attachments):
+    attachments = json.dumps(attachments) # 리스트는 Json 으로 덤핑 시켜야 Slack한테 제대로 간다.
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel, "text": text ,"attachments": attachments})
+
+
+str1_title = f"{Dollar_Index.DollarRate("date")}일 현재 환율정보"
+link = 'https://kr.investing.com/currencies/us-dollar-index'
+str2_text = "환율정보 안내를 시작합니다."
+text = f'''
+환율: {Dollar_Index.DollarRate("num")}
+달러 인덱스: {Dollar_Index.DollarIndex()}
+'''
+
+attach_dict = {
+    'color' : '#ff0000',
+    'author_name' : 'Slack Bot Notice',
+    'title' : str1_title,
+    'title_link' : link,
+    'text' : text
+}
+attach_list = [attach_dict]
 
 # 환경변수 불러오기
 load_dotenv()
@@ -11,5 +37,4 @@ slack_token = os.getenv('API_TOKEN')
 # slack과 파이썬 연결
 client = slack_sdk.WebClient(token = slack_token)
 
-client.chat_postMessage(channel = '#주가예측-프로그램',
-                        text = f'{Dollar_Index.DollarIndex()}')
+notice_message(slack_token, "#주가예측-프로그램", str2_text, attach_list)
